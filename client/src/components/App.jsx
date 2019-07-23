03146c7c673b939388d4eab722a0c585438c330a
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-07-23 21:01:57 +0200

    Implement Hello World React app

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2019-present GCF Task Force. All Rights Reserved.
+ */
+
+import React from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = () => (
+  <div>Hello World!</div>
+);
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2019-present GCF Task Force. All Rights Reserved.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
