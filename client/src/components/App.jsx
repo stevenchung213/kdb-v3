49c093bb1a0f2df4daeb12ff8ed4163427fbcb47
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-09-09 18:15:07 +0700

    Render NavBar and Landing components within App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,11 +4,20 @@
 
 import React from 'react';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
-import Landing from './Landing.jsx';
+import Landing from './Landing';
+import NavBar from './NavBar';
+
+const AppContainer = styled.div`
+  height: 100%;
+`;
 
 const App = () => (
-  <Landing />
+  <AppContainer>
+    <NavBar />
+    <Landing />
+  </AppContainer>
 );
 
 export default App;
