cdbaf48cfd057796ae868d6439814c107d76b0ac
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-09-13 18:24:57 +0700

    Replace stubbed HTML in Map with styled-components

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -11,13 +11,36 @@ const MapGrid = styled.div`
   grid-template-rows: 17fr 3fr;
   align-items: center;
   justify-items: center;
-  background-color: tomato;
+  background-color: white;
+`;
+
+const MapButton = styled.div`
+  background-color: transparent;
+  border: solid black 2px;
+  border-radius: 0;
+  cursor: pointer !important;
+  display: flex;
+  height: 60%;
+  align-items: center;
+  justify-content: center;
+  /* transition: border-color 0.4s ease 0s, background-color 0.4s ease 0s; */
+  width: 25%;
+`;
+
+const MapButtonText = styled.span`
+  /* transition: color 0.4s ease 0s; */
+  color: black;
+  /* font-family: Arial, Helvetica, sans-serif; */
+  font-size: 18px;
+  font-weight: 700;
 `;
 
 const Map = () => (
   <MapGrid>
-    <h1>Map</h1>
-    <button>Navigate Full Database</button>
+    <div />
+    <MapButton>
+      <MapButtonText>Navigate Full Database</MapButtonText>
+    </MapButton>
   </MapGrid>
 );
 
