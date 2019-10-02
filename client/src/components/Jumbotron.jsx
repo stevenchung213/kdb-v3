df732b01660f9a15a7a132edbeeed6c5fc948f63
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-10-02 12:57:55 +0700

    Increase responsiveness of Jumbotron layout + add styles

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -21,22 +21,26 @@ const JumbotronGrid = styled.div`
 
 const JumbotronMiniGrid = styled.div`
   display: grid;
-  grid-template-columns: 11fr 9fr;
-  grid-template-rows: 1fr 2fr 3fr 1fr;
+  grid-template-columns: minmax(460px, 11fr) 9fr;
+  grid-template-rows: 1fr 2.5fr 2.5fr 1fr;
+  /* grid-template-rows: 75px 150px 225px 75px; */
   background-color: rgba(255, 255, 255, 0.75);
-  align-items: center;
-  padding: 10% 0 10% 0;
+  /* align-items: center; */
+  padding: 85px 0 85px 0;
 `;
 
 const JumbotronBrandText = styled.div`
-  height: 100%;
+  /* height: 100%; */
   font-size: 20px;
   font-weight: 700;
   /* line-height: 75px; */
   letter-spacing: 3px;
   padding-left: 30px;
-  text-align: left;
+  padding-top: 10px;
+  /* text-align: left; */
+  align-self: center;
   width: 100%;
+  /* border: 1px dashed red; */
 `;
 
 const JumbotronTitle = styled.div`
@@ -46,8 +50,13 @@ const JumbotronTitle = styled.div`
   /* line-height: 75px; */
   letter-spacing: 3px;
   padding-left: 30px;
+  padding-top: 5px;
   text-align: left;
   width: 100%;
+  font-family: helvetica;
+  font-size: 75px;
+  text-shadow: rgba(0, 0, 0, 0.4) 0px 4px 5px;
+  /* border: 1px dashed red; */
 `;
 
 const JumbotronDescription = styled.div`
@@ -58,8 +67,11 @@ const JumbotronDescription = styled.div`
   letter-spacing: 1px;
   line-height: 30px;
   padding-left: 30px;
+  padding-top: 15px;
   text-align: left;
   width: 100%;
+  line-height: 1.8em;
+  /* border: 1px dashed red; */
 `;
 
 const JumbotronButtonGrid = styled.div`
@@ -68,6 +80,7 @@ const JumbotronButtonGrid = styled.div`
   place-items: center;
   height: 100%;
   width: 100%;
+  /* border: 1px dashed red; */
 `;
 
 const JumbotronButton = styled.div`
@@ -76,19 +89,34 @@ const JumbotronButton = styled.div`
   border-radius: 0;
   cursor: pointer !important;
   display: flex;
+  font-family: 'Avenir Next';
   height: 60%;
   align-items: center;
   justify-content: center;
-  /* transition: border-color 0.4s ease 0s, background-color 0.4s ease 0s; */
+  transition: border 0.6s ease 0s, background-color 0.6s ease 0s, color 0.6s ease 0s;
   width: 100%;
+  &:hover {
+    border: solid #582399 2px;
+    background-color: #582399;
+    /* border: solid #7442b3 2px;
+    background-color: #7442b3; */
+    /* border: solid #693ba3 2px;
+    background-color: #693ba3; */
+    /* color: #582399; */
+    color: white;
+  }
 `;
 
 const JumbotronButtonText = styled.span`
   /* transition: color 0.4s ease 0s; */
-  color: black;
+  /* color: black; */
   /* font-family: Arial, Helvetica, sans-serif; */
   font-size: 18px;
   font-weight: 700;
+  /* transition: color 0.4s ease 0s;
+  &:hover {
+    color: #582399;
+  } */
 `;
 
 const Jumbotron = () => (
