02b6e6dfd97777d694d97923a3b24066360d303c
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-10-02 13:19:54 +0700

    Add hover styles to button in Map component

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -5,6 +5,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import WorldMap from './WorldMap';
+
 const MapGrid = styled.div`
   display: grid;
   /* grid-area: map; */
@@ -23,21 +25,43 @@ const MapButton = styled.div`
   height: 60%;
   align-items: center;
   justify-content: center;
-  /* transition: border-color 0.4s ease 0s, background-color 0.4s ease 0s; */
+  transition: border 0.6s ease 0s, background-color 0.6s ease 0s, color 0.6s ease 0s;
   width: 25%;
+  &:hover {
+    /* border: solid #3E522D 2px;
+    background-color: #3E522D; */
+    border: solid #3E522D 2px;
+    background-color: #3E522D;
+    /* border: solid #426539 2px;
+    background-color: #426539; */
+    /* color: #582399; */
+    color: white;
+  }
 `;
 
 const MapButtonText = styled.span`
   /* transition: color 0.4s ease 0s; */
-  color: black;
+  /* color: black; */
   /* font-family: Arial, Helvetica, sans-serif; */
   font-size: 18px;
   font-weight: 700;
 `;
 
+// const JumbotronButtonText = styled.span`
+// /* transition: color 0.4s ease 0s; */
+// /* color: black; */
+// /* font-family: Arial, Helvetica, sans-serif; */
+// font-size: 18px;
+// font-weight: 700;
+// /* transition: color 0.4s ease 0s;
+// &:hover {
+//   color: #582399;
+// } */
+// `;
+
 const Map = () => (
   <MapGrid>
-    <div />
+    <WorldMap />
     <MapButton>
       <MapButtonText>Navigate Full Database</MapButtonText>
     </MapButton>
