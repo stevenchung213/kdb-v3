f9d01013ea666f905f43bf6d9900e3c8b4834a32
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-10-04 01:28:29 +0700

    Stub all text content for landing page in 5 languages

---
+++ multi-lingual.js
@@ -0,0 +1,172 @@
+module.exports = {
+  en: {
+    navBar: {
+      NAVIGATE_DATABASE: 'Navigate Database',
+      ABOUT: 'About',
+      CONTACT: 'Contact',
+      SEARCH_PLACEHOLDER: 'Search',
+    },
+    landingPage: {
+      jumbotron: {
+        DESCRIPTION: 'An open platform integrating comprehensive information from GCF Task Force member states and provinces reporting current status and trends regarding land use, deforestation, frameworks, policies, and partnerships.',
+        NAVIGATE_DB: 'Navigate DB',
+        GLOBAL_SUMMARY: 'Global Summary',
+      },
+      map: {
+        TITLE: 'GCFTF National Influence',
+        SUB_TITLE: 'By Number of GCFTF States/Provinces',
+        NAVIGATE_FULL_DATABASE: 'Navigate Full Database',
+      },
+      sellingPoints: {
+        TITLE: 'GCFTF Member States Contain:',
+        TEXT_ONE: null,
+        TEXT_TWO: null,
+        TEXT_THREE: null,
+      },
+      footer: {
+        MORE: 'More:',
+        WITH_SUPPORT_FROM: 'With Support From:',
+        ABOUT: ' About',
+        SOURCES: ' Sources',
+        CONTACTS: ' Contacts',
+        BACK_TO_MAIN_SITE: ' Back to Main Site',
+      },
+    },
+  },
+  es: {
+    navBar: {
+      NAVIGATE_DATABASE: 'Navegar base de datos',
+      ABOUT: 'Acerca de',
+      CONTACT: 'Contacto',
+      SEARCH_PLACEHOLDER: 'Buscar',
+    },
+    landingPage: {
+      jumbotron: {
+        DESCRIPTION: 'Una plataforma abierta que integra información integral de los estados miembros y las provincias del Grupo de Trabajo del GCF que informan sobre el estado actual y las tendencias con respecto al uso de la tierra, la deforestación, los marcos, las políticas y las asociaciones.',
+        NAVIGATE_DB: 'Navegar DB',
+        GLOBAL_SUMMARY: 'Resumen Global',
+      },
+      map: {
+        TITLE: 'GCFTF Influencia Nacional',
+        SUB_TITLE: 'Por Número de Estados/Provincias del GCFTF',
+        NAVIGATE_FULL_DATABASE: 'Navegar por la Base de Datos Completa',
+      },
+      sellingPoints: {
+        TITLE: 'Los Estados Miembros del GCFTF Contienen:',
+        TEXT_ONE: null,
+        TEXT_TWO: null,
+        TEXT_THREE: null,
+      },
+      footer: {
+        MORE: 'Más:',
+        WITH_SUPPORT_FROM: 'Con el Apoyo de:',
+        ABOUT: ' Acerca de',
+        SOURCES: ' Fuentes',
+        CONTACTS: ' Contactos',
+        BACK_TO_MAIN_SITE: ' Volver al Sitio Principal',
+      },
+    },
+  },
+  fr: {
+    navBar: {
+      NAVIGATE_DATABASE: 'Naviguer dans la Base de Données',
+      ABOUT: 'À Propos',
+      CONTACT: 'Contact',
+      SEARCH_PLACEHOLDER: 'Chercher',
+    },
+    landingPage: {
+      jumbotron: {
+        DESCRIPTION: 'Une plate-forme ouverte intégrant des informations complètes provenant des États membres et du groupe de travail du GCF, faisant rapport sur l\'état et les tendances en matière d\'utilisation des terres, de déforestation, de cadres, de politiques et de partenariats.',
+        NAVIGATE_DB: 'Naviguer DB',
+        GLOBAL_SUMMARY: 'Résumé Global',
+      },
+      map: {
+        TITLE: 'GCFTF Influence Nationale',
+        SUB_TITLE: 'GCFTF Influence Nationale',
+        NAVIGATE_FULL_DATABASE: 'Naviguer dans la Base de Données Complète',
+      },
+      sellingPoints: {
+        TITLE: 'Les États Membres de la GCFTF Contiennent:',
+        TEXT_ONE: null,
+        TEXT_TWO: null,
+        TEXT_THREE: null,
+      },
+      footer: {
+        MORE: 'Plus:',
+        WITH_SUPPORT_FROM: 'Avec le Soutien de:',
+        ABOUT: ' À Propos',
+        SOURCES: ' Sources',
+        CONTACTS: ' Contacts',
+        BACK_TO_MAIN_SITE: ' Retour au Site Principal',
+      },
+    },
+  },
+  id: {
+    navBar: {
+      NAVIGATE_DATABASE: 'Navigasi Basis Data',
+      ABOUT: 'Tentang',
+      CONTACT: 'Kontak',
+      SEARCH_PLACEHOLDER: 'Cari',
+    },
+    landingPage: {
+      jumbotron: {
+        DESCRIPTION: 'Platform terbuka yang memadukan informasi komprehensif dari negara bagian dan provinsi anggota Gugus Tugas GCF yang melaporkan status dan tren terkini terkait penggunaan lahan, deforestasi, kerangka kerja, kebijakan, dan kemitraan.',
+        NAVIGATE_DB: 'Arahkan DB',
+        GLOBAL_SUMMARY: 'Ringkasan Global',
+      },
+      map: {
+        TITLE: 'Pengaruh Nasional GCFTF',
+        SUB_TITLE: 'Berdasarkan Jumlah Negara/Provinsi GCFTF',
+        NAVIGATE_FULL_DATABASE: 'Menavigasi Basis Data Lengkap',
+      },
+      sellingPoints: {
+        TITLE: 'Negara Anggota GCFTF Berisi:',
+        TEXT_ONE: null,
+        TEXT_TWO: null,
+        TEXT_THREE: null,
+      },
+      footer: {
+        MORE: 'Lebih:',
+        WITH_SUPPORT_FROM: 'Dengan Dukungan Dari:',
+        ABOUT: ' TENTANG',
+        SOURCES: ' Sumber',
+        CONTACTS: ' Kontak',
+        BACK_TO_MAIN_SITE: ' Kembali ke Situs Utama',
+      },
+    },
+  },
+  pt: {
+    navBar: {
+      NAVIGATE_DATABASE: 'Navegar no Banco de Dados',
+      ABOUT: 'Sobre',
+      CONTACT: 'Contato',
+      SEARCH_PLACEHOLDER: 'Procurar',
+    },
+    landingPage: {
+      jumbotron: {
+        DESCRIPTION: 'Uma plataforma aberta que integra informações abrangentes dos estados e províncias membros da Força-Tarefa do GCF, relatando o status atual e as tendências em relação ao uso da terra, desmatamento, estruturas, políticas e parcerias.',
+        NAVIGATE_DB: 'Navegar DB',
+        GLOBAL_SUMMARY: 'Resumo Global',
+      },
+      map: {
+        TITLE: 'Influência Nacional do GCFTF',
+        SUB_TITLE: 'Por Número de Estados/Províncias do GCFTF',
+        NAVIGATE_FULL_DATABASE: 'Navegar no Banco de Dados Completo',
+      },
+      sellingPoints: {
+        TITLE: 'Os Estados Membros da GCFTF Contêm:',
+        TEXT_ONE: null,
+        TEXT_TWO: null,
+        TEXT_THREE: null,
+      },
+      footer: {
+        MORE: 'Mais:',
+        WITH_SUPPORT_FROM: 'Com Suporte de:',
+        ABOUT: ' Sobre',
+        SOURCES: ' Fontes',
+        CONTACTS: ' Contatos',
+        BACK_TO_MAIN_SITE: ' Voltar ao Site Principal',
+      },
+    },
+  },
+};
