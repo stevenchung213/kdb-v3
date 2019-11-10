ed5b993f7d4897b1e2c13a60c533a0cd6c3408e8
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-10 18:53:38 +0700

    Init NJHeader component

---
+++ client/src/components/NJHeader.jsx
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2019-present GCF Task Force. All Rights Reserved.
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+
+const NJHeader = styled.div`
+  background-color: rebeccapurple;
+  height: 100%;
+  width: 100%;
+`;
+
+export default NJHeader;
