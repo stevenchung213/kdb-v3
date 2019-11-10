d881e01fc61b83bdc6669185972a827c29d3c5e3
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-10 18:54:06 +0700

    Implement CSS Grid layout for Nation/Jurisdiction Page container

--- client/src/components/NJPage.jsx
+++ client/src/components/NJPage.jsx
@@ -6,16 +6,27 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
+import NJHeader from './NJHeader';
+import NJNav from './NJNav';
+import NJBody from './NJBody';
+
+// One fragment is ???!
 const NJPageGrid = styled.div`
   display: grid;
   padding-top: 75px;
-  grid-template-rows: 700px 370px;
-  height: 100%;
+  grid-template-rows: 400px 50px auto;
+  /* grid-template-columns: auto; */
+  /* grid-template-areas: "nj-header nj-header"
+                          "nj-nav nj-nav"
+                          "nj-body nj-body"*/
+  height: 1250px;
 `;
 
 const NJPage = () => (
   <NJPageGrid>
-    <div>Hello Nation/Jurisidiction page!</div>
+    <NJHeader>Hello Nation/Jurisidiction page!</NJHeader>
+    <NJNav>Hello Nation/Jurisidiction page!</NJNav>
+    <NJBody>Hello Nation/Jurisidiction page!</NJBody>
   </NJPageGrid>
 );
 
