adbc1786628e600b5f5a0387cca7764567285a8e
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-10 18:53:08 +0700

    Init NJBody component

---
+++ client/src/components/NJBody.jsx
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2019-present GCF Task Force. All Rights Reserved.
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+
+const NJBody = styled.div`
+  background-color: skyblue;
+  height: 100%;
+  width: 100
+`;
+
+export default NJBody;
