47c3917217692c28a90fc035a10832da4a535952
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-10 18:53:53 +0700

    Init NJNav component

---
+++ client/src/components/NJNav.jsx
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2019-present GCF Task Force. All Rights Reserved.
+ */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NJNav = styled.div`
+  background-color: tomato;
+  height: 100%;
+  width: 100%;
+`;
+
+export default NJNav;
