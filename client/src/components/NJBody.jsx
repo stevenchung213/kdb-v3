eb950acafa4bffa411c0232b30ae63ea8c9fbb6e
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-11-16 16:58:45 +0700

    Add useParams React Router Hook and gradient background to NJBody component

--- client/src/components/NJBody.jsx
+++ client/src/components/NJBody.jsx
@@ -3,12 +3,26 @@
  */
 
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
-const NJBody = styled.div`
-  background-color: skyblue;
+const NJBodyStyled = styled.div`
+  background-color: #e5e5e5;
+  /* background-color: #fff;
+  background-image: linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%); */
   height: 100%;
-  width: 100
+  width: 100;
 `;
+
+const NJBody = () => {
+  let { pageId } = useParams();
+  if (!pageId) {
+    pageId = 'No ID!';
+  }
+
+  return (
+    <NJBodyStyled>{`Hello Nation/Jurisidiction ${pageId} page!`}</NJBodyStyled>
+  );
+}
 
 export default NJBody;
