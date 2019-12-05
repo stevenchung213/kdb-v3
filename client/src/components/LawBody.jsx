8c7fb8be25dfba4a06f24285e878c8e52c694960
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-12-05 20:44:27 +0700

    Add link to S3 hosted legal documents in LawBody component

--- client/src/components/LawBody.jsx
+++ client/src/components/LawBody.jsx
@@ -49,7 +49,7 @@ const LawBodySummaryText = styled.div`
   font-size: 14px;
 `;
 
-const Icon2 = styled.i`
+const Icon2 = styled.a`
   align-self: end;
   justify-self: right;
 `;
@@ -70,7 +70,7 @@ const LawBody = ({ isOpen }) => {
         <LawBodySummaryTitle>Summary:</LawBodySummaryTitle>
         <LawBodySummaryText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in mauris quam. In semper dolor vel nunc porttitor ornare. Maecenas hendrerit urna euismod, sodales orci eget, pulvinar risus. Proin lacinia tincidunt ante, quis feugiat ipsum accumsan id. Sed facilisis urna nisl, in ultricies turpis fermentum eget. Nullam turpis libero, venenatis eu urna eget, dapibus varius mauris. Integer vehicula porttitor vestibulum. Nunc bibendum tortor id egestas commodo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam pharetra eleifend felis. Praesent commodo risus nec aliquet maximus. Mauris bibendum volutpat dui. Pellentesque at cursus arcu. Pellentesque consequat aliquet faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</LawBodySummaryText>
       </LawBodySummary>
-      <Icon2 className="far fa-file-pdf fa-4x" />
+      <Icon2 className="far fa-file-pdf fa-4x" href="https://laws-and-policies.s3-us-west-1.amazonaws.com/brazil/brasil-mato-redd%2Bcouncil.pdf" rel="noopener noreferrer" target="_blank" />
     </LawBodyGrid>
   );
 };
