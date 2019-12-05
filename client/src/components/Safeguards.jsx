826c0cbf5e55bda20a768e66007275755780c29f
Anthony Pecchillo <anthonypecchillo@gmail.com>
2019-12-05 20:46:57 +0700

    Hardcode content into Safeguards section of NJGovernance page

--- client/src/components/Safeguards.jsx
+++ client/src/components/Safeguards.jsx
@@ -19,7 +19,7 @@ const SAFEGUARDS_TAB_LABELS = [
 const SafeguardsGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 37px 1000px;
+  grid-template-rows: 37px calc(100% - 5% - 15px);
 
   height: 100%;
   width: 100%;
--- client/src/components/TransparencyAndParticipation.jsx
+++ client/src/components/TransparencyAndParticipation.jsx
@@ -5,18 +5,81 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const content = (
+  <div><p><strong>Transparency and participation mechanisms</strong></p>
+  <p>&nbsp;</p>
+  <p><strong>1. Actions taken to guarantee free, prior and informed consent</strong></p>
+  <p>The draft Environmental Services Incentives System (SISA) law creates an institutional arrangement that includes a Hearing Body to receive claims and mediate conflicts. Another level of participation and social control is represented by the Commission for Validation and Monitoring (CEVA) formed by government and non-government representatives chosen among members of the 3 State Councils (Environment, Forest, and Rural Development).</p>
+  <p>&nbsp;</p>
+  <p>In addition to CEVA and Councils, the Climate Change and Environmental Service Regulation Institute (IMC) formed the Indigenous Work Group with indigenous leaderships, state and federal government bodies responsible for policies related to indigenous lands, as well as local and international NGOs working with indigenous populations. This work group is an advisory body of IMC and CEVA to guide appropriate actions related to awareness and continuing education on environmental services and the IES Carbon Program for adequate participation and decision-making by indigenous peoples.&nbsp;</p>
+  <p>&nbsp;</p>
+  <p><strong>2. Mechanisms for consultation and continuous participation addressed</strong></p>
+  <ul>
+  <li>Advisory process for development of the IES Carbon Program and the Law establishing the Incentives for Environmental Services System:</li>
+  <li>Invitation letters for the consultation process sent to 120 individuals from more than 72 local, national, and international organizations</li>
+  </ul>
+  <ul>
+  <li>Meetings: 5 meetings with 40 individuals from local organizations</li>
+  </ul>
+  <ul>
+  <li>Workshops: 3 3-day workshops attended by 80 potential beneficiaries</li>
+  </ul>
+  <ul>
+  <li>Seminar: 32 people from 10 national and international civil society organizations and 7 Government Secretaries</li>
+  </ul>
+  <ul>
+  <li>Written feedback: 3 written comments sent by email</li>
+  </ul>
+  <ul>
+  <li>IES Carbon Program implementation phase:</li>
+  </ul>
+  <ul>
+  <li>3 State Councils as a whole: established by Law #2.308/2010 to conduct joint deliberations on the SISA and the IES Carbon Program and monitor CEVA&rsquo;s actions.</li>
+  </ul>
+  <ul>
+  <li>Commission for Validation and Monitoring (CEVA): - SISA and IES Carbon Program supervision.</li>
+  </ul>
+  <ul>
+  <li>Ombudsman of SISA and IES Carbon Program: claims and complaints regarding program execution.</li>
+  </ul>
+  <p>&nbsp;</p>
+  <p>The consultation process will be a continuous, iterative process. Therefore, the sub-programs and projects of the IES Program will be designed in a participatory manner with the beneficiaries and will be supervised by a Local Follow-up Committee composed of representatives of the beneficiaries.</p>
+  <p>&nbsp;</p>
+  <p><strong>3. Information on transparency of REDD program</strong></p>
+  <ul>
+  <li>Available information: Document describing the first version of the program (printed and digital)</li>
+  </ul>
+  <ul>
+  <li>Media used: Email (invitation for participating in consultation process) and Government website during the consultation stage in order to design the IES-Carbon Program and the SISA.</li>
+  </ul>
+  <ul>
+  <li>Public access: Government website with disclosure of Law and consultation documentation in the implementation stage such as the consultation of indicators for monitoring social and environmental safeguards for REDD+.&nbsp; </li>
+  </ul>
+  <ul>
+  <li>REDD Observatory: ongoing monitoring of SISA and its programs.&nbsp;</li>
+  </ul>
+  <p>&nbsp;</p>
+  <p><strong>4. Needs identified for improvement in participation and transparency</strong></p>
+  <ul>
+  <li>Strengthening the State Validation and Monitoring Commission for the Incentives for Environmental Services System with regards to capacity-building on the theme of participation and transparency in order to be able to fulfill the expected role (approval of REDD sub-programs and norms).</li>
+  </ul>
+  <ul>
+  <li>Webpage of the Climate Change and Environmental Service Regulation Institute for publication of information about the Carbon program and Incentives for Environmental Services System.</li>
+  </ul></div>
+);
+
 const TransparencyAndParticipationStyled = styled.div`
-  background-color: white;
   border-top: 3px solid #3E522D;
-  height: calc(100% - 5% - 37px - 15px);
+  height: calc(100% - 2.5%);
+  margin-top: 15px;
   overflow-y: scroll;
+  padding: 15px;
   width: 100%;
-  margin: 15px 0;
 `;
 
 const TransparencyAndParticipation = () => (
   <TransparencyAndParticipationStyled>
-    Transparency And Participation
+    {content}
   </TransparencyAndParticipationStyled>
 );
 
